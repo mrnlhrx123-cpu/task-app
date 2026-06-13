6f0cc77426fda16412013af9ec1d15c8b8ad1ae7
Marouan <mrnlhrx123@gmail.com>
2026-06-13 19:38:30 +0100

    Fix apps.py configuration

--- tasks/models.py
+++ tasks/models.py
@@ -9,7 +9,8 @@ class Task(models.Model):
     elapsed_seconds = models.IntegerField(default=0) # المؤقت الفعلي بالثواني
     status = models.CharField(max_length=15, default='planned') # planned, completed, postponed
     category = models.CharField(max_length=100)
-description = models.TextField(blank=True, null=True)
-start_time = models.TimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    start_time = models.TimeField(blank=True, null=True)
+
     def __str__(self):
         return self.text
