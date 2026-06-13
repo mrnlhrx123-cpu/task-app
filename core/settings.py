03a2798d8dcbfa8574d3a8b99514b1165c884146
Marouan <mrnlhrx123@gmail.com>
2026-06-13 16:15:41 +0100

    Fix apps.py configuration

--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CORS_ALLOW_ALL_ORIGINS = True # باش الـ JS يقدر يتواصل مع Django بلا حظر
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
