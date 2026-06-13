7f699fb5d2d9c2c689d40cca71b6743f0414e185
Marouan <mrnlhrx123@gmail.com>
2026-06-13 16:49:45 +0100

    Fix apps.py configuration

--- core/settings.py
+++ core/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d5ca8e4rvwdu@@jv@m81^_eaum8tsd_v85rf8zwf#a($d&s+xi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
+# بدليها مؤقتاً لـ True
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
