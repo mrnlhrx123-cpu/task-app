46b580032d3b4ff40efc2243279ba8a71aa05f8b
Marouan <mrnlhrx123@gmail.com>
2026-06-13 15:58:36 +0100

    update code and push to render

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
 
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
 class TasksConfig(AppConfig):
     name = 'tasks'
+class YourAppConfig(AppConfig):
+    name = 'your_app_name'
+
+    def ready(self):
+        post_migrate.connect(create_superuser, sender=self)
+
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'password123')
